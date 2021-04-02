42f1cce1c18bfaa87f7c677446147c038a6b3339
rawk77 <Chsinnott@gmail.com>
2021-04-02 11:17:10 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,9 +9,10 @@ echo ' '
 echo '======= Setup Script ========'
 
 # Create required Directories if they don't exist
-if [ -d ~/.local/bin ]; then echo ".local/bin Exists"; else mkdir ~/.local/bin; fi
-if [ -d ~/.local/icons ]; then echo ".local/icons"; else mkdir ~/.local/icons; fi
-if [ -d ~/.local/share/applications ]; then echo ".local/share/applications Exists"; else mkdir -p ~/.local/share/applications; fi
+if [ -d ~/.local/bin ]; then echo "[+] .local/bin Exists"; else mkdir ~/.local/bin; fi
+if [ -d ~/.local/icons ]; then echo "[+] .local/icons"; else mkdir ~/.local/icons; fi
+if [ -d ~/.local/share/applications ]; then echo "[+] .local/share/applications Exists"; else mkdir -p ~/.local/share/applications; fi
+if [ -d ~/.vim/colors ]; then echo "[+] .vim/colors Exists"; else mkdir -p ~/.vim/colors; fi
 
 # Setup Obsidian
 curl https://github.com/obsidianmd/obsidian-releases/releases/download/v0.11.9/Obsidian-0.11.9.AppImage -o ~/.local/bin/Obsidian
@@ -26,4 +27,8 @@ curl https://raw.githubusercontent.com/rawk77/blob/master/tmux.conf -o ~/.tmux.c
 curl https://raw.githubusercontent.com/rawk77/blob/master/vimrc -o ~/.vimrc
 curl https://raw.githubusercontent.com/rawk77/blob/master/vpnpanel.sh -o ~/.local/vpnpanel.sh
 chmod +x ~/.local/vpnpanel.sh
+
+# Vim Color Schemes
+curl https://raw.githubusercontent.com/sjl/badwolf/master/colors/badwolf.vim -o ~/.vim/colors/badwolf.vim
+curl https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/colors/solarized.vim -o ~/.vim/colors/solarized.vim
 
