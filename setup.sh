f00b7e635389f6c0bc3584ca1602a98f33c93752
rawk77 <Chsinnott@gmail.com>
2021-04-02 11:27:57 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,17 +15,17 @@ if [ -d ~/.local/share/applications ]; then echo "[+] .local/share/applications 
 if [ -d ~/.vim/colors ]; then echo "[+] .vim/colors Exists"; else mkdir -p ~/.vim/colors; fi
 
 # Setup Obsidian
-curl https://github.com/obsidianmd/obsidian-releases/releases/download/v0.11.9/Obsidian-0.11.9.AppImage -o ~/.local/bin/Obsidian
+wget https://github.com/obsidianmd/obsidian-releases/releases/download/v0.11.9/Obsidian-0.11.9.AppImage -O ~/.local/bin/Obsidian
 chmod +x ~/.local/bin/Obsidian
-curl https://github.com/rawk77/blob/raw/master/Obsidian.png -o ~/.local/icons/Obsidian.png
-curl https://raw.githubusercontent.com/rawk77/blob/master/Obsidian.desktop -o ~/.local/share/applications/Obsidian.desktop
+wget https://github.com/rawk77/blob/raw/master/Obsidian.png -O ~/.local/icons/Obsidian.png
+wget https://raw.githubusercontent.com/rawk77/blob/master/Obsidian.desktop -O ~/.local/share/applications/Obsidian.desktop
 
 # Setup dotfiles
 echo 'Downloading configuration files'
-curl https://raw.githubusercontent.com/rawk77/blob/master/gdbinit -o ~/.gdbinit
-curl https://raw.githubusercontent.com/rawk77/blob/master/tmux.conf -o ~/.tmux.conf
-curl https://raw.githubusercontent.com/rawk77/blob/master/vimrc -o ~/.vimrc
-curl https://raw.githubusercontent.com/rawk77/blob/master/vpnpanel.sh -o ~/.local/vpnpanel.sh
+curl -s https://raw.githubusercontent.com/rawk77/blob/master/gdbinit -o ~/.gdbinit
+curl -s https://raw.githubusercontent.com/rawk77/blob/master/tmux.conf -o ~/.tmux.conf
+curl -s https://raw.githubusercontent.com/rawk77/blob/master/vimrc -o ~/.vimrc
+curl -s https://raw.githubusercontent.com/rawk77/blob/master/vpnpanel.sh -o ~/.local/vpnpanel.sh
 chmod +x ~/.local/vpnpanel.sh
 
 # Vim Color Schemes
