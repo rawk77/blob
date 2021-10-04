a233b62455d8a7589f971c8625f092acc0545326
rawk77 <Chsinnott@gmail.com>
2021-10-04 15:55:37 -0400

    Removed Obsidian Setup

--- setup.sh
+++ setup.sh
@@ -9,16 +9,7 @@ echo ' '
 echo '======= Setup Script ========'
 
 # Create required Directories if they don't exist
-if [ -d ~/.local/bin ]; then echo "[+] .local/bin Exists"; else mkdir ~/.local/bin; fi
-if [ -d ~/.local/icons ]; then echo "[+] .local/icons"; else mkdir ~/.local/icons; fi
-if [ -d ~/.local/share/applications ]; then echo "[+] .local/share/applications Exists"; else mkdir -p ~/.local/share/applications; fi
 if [ -d ~/.vim/colors ]; then echo "[+] .vim/colors Exists"; else mkdir -p ~/.vim/colors; fi
-
-# Setup Obsidian
-wget https://github.com/obsidianmd/obsidian-releases/releases/download/v0.11.9/Obsidian-0.11.9.AppImage -O ~/.local/bin/Obsidian
-chmod +x ~/.local/bin/Obsidian
-wget https://github.com/rawk77/blob/raw/master/Obsidian.png -O ~/.local/icons/Obsidian.png
-wget https://raw.githubusercontent.com/rawk77/blob/master/Obsidian.desktop -O ~/.local/share/applications/Obsidian.desktop
 
 # Setup dotfiles
 echo 'Downloading configuration files'
@@ -31,4 +22,7 @@ chmod +x ~/.local/vpnpanel.sh
 # Vim Color Schemes
 curl https://raw.githubusercontent.com/sjl/badwolf/master/colors/badwolf.vim -o ~/.vim/colors/badwolf.vim
 curl https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/colors/solarized.vim -o ~/.vim/colors/solarized.vim
+
+# Vim-Plug Install
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
