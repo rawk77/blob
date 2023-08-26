ce777f4f4f868b0690a644aba4d74dadcc297605
rawk77 <Chsinnott@gmail.com>
2023-08-26 00:42:22 -0400

    Create init.el

---
+++ emacs/init.el
@@ -0,0 +1,52 @@
+(setq inhibit-startup-screen t)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(tooltip-mode -1)
+(menu-bar-mode -1)
+
+(setq visible-bell t)
+(setq ring-bell-function 'ignore)
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Fonts
+(add-to-list 'default-frame-alist
+	     '(font . "LiterationMono Nerd Font-12"))
+(set-face-attribute 'default nil :font "LiterationMono Nerd Font-12" )
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("f5e666fba0ded6ae9be004314ecf5f7feb605cdb84711b5c5ffd81acfb831183" "8721f7ee8cd0c2e56d23f757b44c39c249a58c60d33194fe546659dabc69eebd" default))
+ '(package-selected-packages '(catppuccin-theme doom-modeline dracula-theme ##)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(load-theme 'catppuccin :no-config)
+
+;; Enable vertico
+(use-package vertico
+  :ensure t
+  :init
+  (vertico-mode))
