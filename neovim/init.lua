0cc7622fb094b0584eb379206fbc6d1432368d0c
rawk77 <Chsinnott@gmail.com>
2023-09-08 08:52:24 -0400

    Update init.lua

--- neovim/init.lua
+++ neovim/init.lua
@@ -25,6 +25,8 @@ keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
 keymap("n", "<leader>ft", ":Telescope live_grep<CR>", opts)
 keymap("n", "<leader>fp", ":Telescope projects<CR>", opts)
 keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
+keymap("n", "<leader>fo", ":Telescope oldfiles<CR>", opts)
+keymap("n", "<leader>fh", ":Telescope help_tags<CR>", opts)
 
 -- NeoTree keymap
 keymap("n", "<C-f>", ":Neotree toggle<CR>", opts)
