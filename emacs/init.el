294b131237f48f00fa3c6b8e2e138dfa2d4dbf31
rawk77 <Chsinnott@gmail.com>
2025-02-14 12:48:44 -0500

    Update init.el

--- emacs/init.el
+++ emacs/init.el
@@ -1,22 +1,19 @@
 (setq inhibit-startup-screen t)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(tooltip-mode -1)
-(menu-bar-mode -1)
-(linum-mode)
-
+;;(menu-bar-mode 0)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+(global-display-line-numbers-mode)
 (setq visible-bell t)
 (setq ring-bell-function 'ignore)
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-
-;; Copy to System clipboard
-(setq x-select-enable-clipboard t)
+;;(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+(global-hl-line-mode 1)
 
 ;; Fonts
 (add-to-list 'default-frame-alist
 	     '(font . "LiterationMono Nerd Font-11"))
 (set-face-attribute 'default nil :font "LiterationMono Nerd Font-11" )
 
+;; Add MELPA package repo
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
@@ -37,10 +34,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(gruber-darker))
  '(custom-safe-themes
-   '("02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" "f5e666fba0ded6ae9be004314ecf5f7feb605cdb84711b5c5ffd81acfb831183" "8721f7ee8cd0c2e56d23f757b44c39c249a58c60d33194fe546659dabc69eebd" default))
+   '("11819dd7a24f40a766c0b632d11f60aaf520cf96bd6d8f35bae3399880937970" "e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7" default))
  '(package-selected-packages
-   '(vertico doom-themes marginalia catppuccin-theme doom-modeline dracula-theme ##)))
+   '(dracula-theme embark-consult embark orderless consult lsp-mode corfu all-the-icons doom-modeline marginalia vertico gruber-darker-theme)))
+ 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -48,16 +47,6 @@
  ;; If there is more than one, they won't work right.
  )
 
-;; Set Theme
-(use-package catppuccin-theme
-  :ensure t)
-
-;; Only load the theme when emacs is run in GUI
-(when (display-graphic-p)
-  (load-theme 'catppuccin :no-config))
-
-
-;;(load-theme 'catppuccin :no-config)
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1))
@@ -84,3 +73,132 @@
   (marginalia-annotators '(marginalia-annotators-heavy marginalia-annotators-light nil))
   :init
   (marginalia-mode))
+
+;; Orderless adds out of order matching
+(use-package orderless
+  :ensure t
+  :custom
+  (completion-styles '(orderless basic))
+  (completion--category-override '((file (style basic partial-completion)))))
+
+;; Embark adds minibar actions
+(use-package embark
+  :ensure t
+  :bind
+  (("C-." . embark-act)
+   ("C-;" . embark-dwim))
+  :init
+  :config
+  )
+
+(use-package embark-consult
+  :ensure t
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
+
+;; Consult
+(use-package consult
+  ;; Replace bindings. Lazily loaded by `use-package'.
+  :bind (;; C-c bindings in `mode-specific-map'
+         ("C-c M-x" . consult-mode-command)
+         ("C-c h" . consult-history)
+         ("C-c k" . consult-kmacro)
+         ("C-c m" . consult-man)
+         ("C-c i" . consult-info)
+         ([remap Info-search] . consult-info)
+         ;; C-x bindings in `ctl-x-map'
+         ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
+         ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
+         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+         ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+         ("C-x t b" . consult-buffer-other-tab)    ;; orig. switch-to-buffer-other-tab
+         ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
+         ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
+         ;; Custom M-# bindings for fast register access
+         ("M-#" . consult-register-load)
+         ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+         ("C-M-#" . consult-register)
+         ;; Other custom bindings
+         ("M-y" . consult-yank-pop)                ;; orig. yank-pop
+         ;; M-g bindings in `goto-map'
+         ("M-g e" . consult-compile-error)
+         ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
+         ("M-g g" . consult-goto-line)             ;; orig. goto-line
+         ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
+         ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
+         ("M-g m" . consult-mark)
+         ("M-g k" . consult-global-mark)
+         ("M-g i" . consulte-imenu)
+         ("M-g I" . consult-imenu-multi)
+         ;; M-s bindings in `search-map'
+         ("M-s d" . consult-find)                  ;; Alternative: consult-fd
+         ("M-s c" . consult-locate)
+         ("M-s g" . consult-grep)
+         ("M-s G" . consult-git-grep)
+         ("M-s r" . consult-ripgrep)
+         ("M-s l" . consult-line)
+         ("M-s L" . consult-line-multi)
+         ("M-s k" . consult-keep-lines)
+         ("M-s u" . consult-focus-lines)
+         ;; Isearch integration
+         ("M-s e" . consult-isearch-history)
+         :map isearch-mode-map
+         ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
+         ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
+         ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
+         ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
+         ;; Minibuffer history
+         :map minibuffer-local-map
+         ("M-s" . consult-history)                 ;; orig. next-matching-history-element
+         ("M-r" . consult-history))                ;; orig. previous-matching-history-element
+  )
+
+;; Corfu Configuration for auto-completions
+(use-package corfu
+  ;; Optional customizations
+  :custom
+  (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+  (corfu-auto t)                 ;; Enable auto completion
+  (corfu-auto-prefix 2)          ;; ??
+  (corfu-auto-delay 1.0)         ;; Set the delay on auto completion
+  (corfu-quit-at-boundary 'seperator)   ;; Never quit at completion boundary
+  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
+  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
+  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+
+  ;; Enable Corfu only for certain modes. See also `global-corfu-modes'.
+  ;; :hook ((prog-mode . corfu-mode)
+  ;;        (shell-mode . corfu-mode)
+  ;;        (eshell-mode . corfu-mode))
+  :bind (:map corfu-map
+	      ("M-SPC" . corfu-insert-seperator)
+	      ;;("RET"   . nil)
+	      ("TAB"   . corfu-next)
+	      ([tab]   . corfu-next)
+	      ("S-TAB" . corfu-previous)
+	      ([backtab] . corfu-previous)
+	      ("S-<return>" . corfu-insert))
+  ;; Recommended: Enable Corfu globally.  This is recommended since Dabbrev can
+  ;; be used globally (M-/).  See also the customization variable
+  ;; `global-corfu-modes' to exclude certain modes.
+  :init
+  (global-corfu-mode))
+
+;; handle windows
+(setq display-buffer-alist
+      '(("\\*Occur\\*"   ;; force occur to open the window below the current window
+	 (display-buffer-reuse-window
+	  display-buffer-below-selected)
+	 (window-height . fit-window-to-buffer)
+	 (dedicated . t))
+	))
+
+(global-set-key (kbd "<f12>") (lambda () (interactive)
+  (dired "//dbg.local/shares/Support")
+  (message "Opened:  %s" (buffer-name))))
+
+;;(add-hook 'c-mode-hook 'lsp)
+(add-hook 'c-mode-hook 'eglot-ensure)
+(add-hook 'c++-mode-hook 'eglot-ensure)
+;;(add-hook 'c++-mode-hook 'lsp)
